cf026beac57e1248706edb73a8674e51b5eb34b5
mwhite <none@none>
2019-06-13 06:18:40 -0700

    git config update for windows; dir colors update; notion mouse fixes

--- .notion/cfg_mouse.lua
+++ .notion/cfg_mouse.lua
@@ -3,7 +3,7 @@
 -- (Canaan Hadley-Voth)
 --
 -- A set of bindings intended to make the mouse pull its weight.
--- Fluxbox users should feel right at home.  This will be less 
+-- Fluxbox users should feel right at home.  This will be less
 -- useful without a mouse wheel, but still worth considering.
 --
 -- Partly it's an experiment to see how far one can get
@@ -14,6 +14,8 @@
 --
 -- To use, add to cfg_ion.lua: dopath("cfg_mouse")
 
+-- 4 Mar 2019 mwhite: disable scroll wheel for switching workspaces or tabs; can still use Mod1(+Shift)+wheel
+
 defbindings("WScreen", {
 
     -- Switching workspaces can be done from anywhere using Mod1 and
@@ -23,8 +25,8 @@ defbindings("WScreen", {
     --
     -- (Although a couple of the looks don't have any space at edges
     --  of the screen, most of them do.)
-    mpress("Button4", "WScreen.switch_prev(_)"),
-    mpress("Button5", "WScreen.switch_next(_)"),
+    --mpress("Button4", "WScreen.switch_prev(_)"),
+    --mpress("Button5", "WScreen.switch_next(_)"),
     mpress(MOD1.."Button4", "WScreen.switch_prev(_)"),
     mpress(MOD1.."Button5", "WScreen.switch_next(_)"),
 
@@ -46,15 +48,15 @@ defbindings("WFrame", {
     mpress(MOD1.."Button2", "_sub:set_tagged('toggle')"),
     bdoc("Attach tagged regions."),
     mpress(MOD1.."Shift+Button2", "_:attach_tagged()"),
-    
-    -- For mouse users I highly recommend button2 as a mainmenu, 
+
+    -- For mouse users I highly recommend button2 as a mainmenu,
     -- otherwise it would be difficult to start programs using the mouse.
     -- In Ion it's relevant which frame has focus when you start
     -- something, so a file manager would work poorly for that purpose
     -- unless it happened to be in the right frame.
     mpress("Button1@client", "mod_menu.pmenu(_, _sub, 'mainmenu')"),
     mpress("Button2@border", "mod_menu.pmenu(_, _sub, 'mainmenu')"),
-    
+
     bdoc("Maximize vertical on double click"),
     mdblclick("Button1", "WFrame.maximize_vert(_)"),
 
@@ -62,20 +64,20 @@ defbindings("WFrame", {
     -- Oddly enough, it still works if you've hidden the tab bar.
     -- Just use the wheel over the top edge of the screen.
     -- Or use Mod4 and the wheel to avoid having to aim at anything.
-    mpress("Button5@tab", "WScreen.switch_next(_)"),
-    mpress("Button4@tab", "WScreen.switch_prev(_)"),
+    --mpress("Button5@tab", "WScreen.switch_next(_)"),
+    --mpress("Button4@tab", "WScreen.switch_prev(_)"),
 
     -- switch between workspaces; don't quite understand, since the
     --  above code switches between tabs!
     -- added @border to suppress in empty frames and, most importantly,
     --  1 pixel border between frame and tabs
-    mpress("Button5@border", "_:parent():switch_next()"),
-    mpress("Button4@border", "_:parent():switch_prev()"),
+    --mpress("Button5@border", "_:parent():switch_next()"),
+    --mpress("Button4@border", "_:parent():switch_prev()"),
 
     -- MOD + wheel anywhere switches workspaces
     mpress(MOD1.."Button4", "_:parent():switch_prev()"),
     mpress(MOD1.."Button5", "_:parent():switch_next()"),
-    
+
     bdoc("Drag without aiming for a tab, using Mod1+Shift"),
     mdrag(MOD1.."Shift+Button1", "WFrame.p_tabdrag(_)"),
 
@@ -85,14 +87,14 @@ defbindings("WFrame", {
     bdoc("Switch the frame to display the object indicated by the tab."),
     mclick("Button1@tab", "WFrame.p_switch_tab(_)"),
     mclick("Button2@tab", "WFrame.p_switch_tab(_)"),
-    
+
     bdoc("Resize the frame."),
     mdrag("Button1@border", "WFrame.p_resize(_)"),
     mdrag(MOD1.."Button3", "WFrame.p_resize(_)"),
-    
+
     bdoc("Move the frame."),
     mdrag(MOD1.."Button1", "WFrame.p_move(_)"),
-    
+
     bdoc("Move objects between frames by dragging and dropping the tab."),
     mdrag("Button1@tab", "WFrame.p_tabdrag(_)"),
 
@@ -110,7 +112,7 @@ if MOD4 then
 defbindings("WFrame", {
     -- There were only a few bindings involving MOD4, so
     -- I've fixed this to work with or without.
-    -- On my system it's the windows key, defined in 
+    -- On my system it's the windows key, defined in
     -- ~/.ion3/cfg_ion.lua as MOD4="Mod4+"
     bdoc("Show/hide tab"),
     mpress(MOD4.."Button1", "WFrame.set_tabbar(_, 'toggle')"),
@@ -132,7 +134,7 @@ defbindings("WFrame", {
     mpress(MOD1.."Shift+Button5", "WScreen.switch_next(_)"),
     mpress(MOD1.."Shift+Button4", "WScreen.switch_prev(_)"),
 })
-end    
+end
 
 if mod_ionws then
 defbindings("WFrame-on-WIonWS", {
@@ -150,11 +152,11 @@ defbindings("WScratchpad", {
     -- button3 is still a frame context menu
     --
     -- What's missing?  mainmenu via mouse, as described above.
-    -- 
+    --
     -- Why is that not important?  Because you can button2 on any tab
     -- or border in the background to bring up the main menu, and any
     -- applications you start will land in the scratchpad.  They can't
-    -- go anywhere else.  
+    -- go anywhere else.
     mdrag("Button1@tab", "_:p_move()"),
     mdrag("Button2@tab", "_:p_tabdrag()"),
     mclick("Button2", "mod_sp.set_shown_on(_:screen_of(), 'toggle')"),
@@ -184,7 +186,7 @@ end
 --
 --[[
     menuentry("Toggle Tab", "WFrame.set_tabbar(_, 'toggle')"),
-    menuentry("Fullscreen", "WClientWin.set_fullscreen(_sub, 'toggle')", 
+    menuentry("Fullscreen", "WClientWin.set_fullscreen(_sub, 'toggle')",
                 "_sub:WClientWin"),
     menuentry("Show Dock", "mod_dock.set_floating_shown_on(_:screen_of(), 'toggle')"),
     menuentry("Show SP", "mod_sp.set_shown_on(_:screen_of(), 'toggle')"),
@@ -192,7 +194,7 @@ end
 
 
 -- This is a gratuitous alternative way of displaying the scratchpad
--- with a mouse click.  The dockapp wmnet takes an argument for a command 
+-- with a mouse click.  The dockapp wmnet takes an argument for a command
 -- to be executed on a button1 click.  The command reaches into the running
 -- Ion3 and toggles the scratchpad.
 --
